7e9bd595b21dd5c62caa0cdbfae8e429d4faa670
Cole Meier <colemeier@Coles-MacBook-Pro-8.local>
2025-09-08 13:20:40 -0700

    Initial Build

---
+++ src/main/java/com/fithero/fithero/FitheroApplication.java
@@ -0,0 +1,13 @@
+package com.fithero.fithero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitheroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitheroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fithero/fithero/FitheroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fithero.fithero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitheroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
